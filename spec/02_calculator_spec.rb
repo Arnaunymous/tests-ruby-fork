4d8ec9f0c32b41373fd2245301be53ac81dfc26e
BSarahB <bslajta@gmail.com>
2019-10-07 13:31:18 +0200

    completing instructions for testing

--- spec/02_calculator_spec.rb
+++ spec/02_calculator_spec.rb
@@ -44,16 +44,16 @@ end
 describe "#multiply" do
 
   it "multiplies two numbers" do
-    expect(multiply(3, 4)).to eq(12)
+    expect(multiply(3,4)).to eq(12)
   end
 
   it "multiplies two other numbers" do
-    expect(multiply(6, 8)).to eq(14)
+    expect(multiply(6,8)).to eq(14)
   end
     # one other test here, don't forget do end ;)
 
   it "multiplies two other numbers, one of them being 0" do
-    expect(multiply(0, 4)).to eq(0)
+    expect(multiply(0,4)).to eq(0)
   end
     # one last test, with 0 in it
 
