709e8051b4c021a20b11d1bfecf4c8f0a8716721
BSarahB <bslajta@gmail.com>
2019-10-07 20:24:50 +0200

    adding 04_simon_says.rb

--- lib/04_simon_says.rb
+++ lib/04_simon_says.rb
@@ -1,11 +1,42 @@
-  def echo(word)
-    return "#{word}"
-  end
 
-  def shout(word)
-    return word.upcase
-  end
+def echo(string)
+    return string
+end
 
-  def repeat(word)
-    return "#{word}"*2
-  end
+def shout(string)
+    return string.upcase
+end
+
+
+
+def repeat(text, n=2)
+ return [text] * n * ' '
+end
+
+#Cette solution marche aussi
+
+#def repeat(text, c=2)
+#  return c.times.collect { text }.join(' ')
+#end
+
+def start_of_word (string, number)
+ return string [0...number]
+end
+
+def first_word(string)
+    return string.split.first
+end
+
+def titleize(sentence)
+  
+ littlewords = ["the", "and"]
+  array_of_sentence = sentence.capitalize.split(" ").map{|word| 
+    if littlewords.include?(word) 
+    word = word
+    else
+      word = word.capitalize
+    end
+  }
+return array_of_sentence.join(" ")
+
+end
